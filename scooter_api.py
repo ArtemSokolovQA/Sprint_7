a7c5cbcf91d267c1b6cc59a433cf49d9afcae579
Artem Sokolov <ArtemSokolovQA@yandex.ru>
2024-09-04 13:17:12 +0300

    Тест невозможности создания двух курьеров с одинаковыми данными

--- scooter_api.py
+++ scooter_api.py
@@ -6,13 +6,13 @@ import urls
 
 class ScooterApi:
 
-    @allure.step('Создать курьера')
     @staticmethod
+    @allure.step('Создать курьера')
     def create_courier(body):
         return requests.post(f'{urls.BASE_URL}{urls.REGISTER_COURIER_PATH}', json=body)
 
-    @allure.step('Удалить курьера')
     @staticmethod
+    @allure.step('Удалить курьера')
     def delete_courier(courier_id):
         return requests.delete(f'{urls.BASE_URL}{urls.DELETE_COURIER_PATH}', params=courier_id)
 
